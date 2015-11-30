ff431ec201e944cd03cbb88277d5fb6739a0de72
Nándor Holozsnyák <holi60@gmail.com>
2015-11-30 12:20:35 +0100

    project structure, core, service, service-client-api, web, ear

---
+++ service/src/main/java/hu/schonherz/java/courier/service/helloworld/HelloWorldBean.java
@@ -0,0 +1,20 @@
+package hu.schonherz.java.courier.service.helloworld;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+@LocalBean
+public class HelloWorldBean {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldBean() {
+     
+    }
+
+}
