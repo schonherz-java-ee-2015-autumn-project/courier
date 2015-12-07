1d9f85de6e1255655303fa9bf2be18a8023615a5
SzPapellas <szabolcspapellas@gmail.com>
2015-12-07 21:47:29 +0100

    dao

---
+++ core/src/main/java/hu/schonherz/java/training/courier/dao/CargoDao.java
@@ -0,0 +1,11 @@
+package hu.schonherz.java.training.courier.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import hu.schonherz.java.training.courier.entities.Cargo;
+
+@Repository
+public interface CargoDao extends JpaRepository<Cargo, Long> {
+
+}
