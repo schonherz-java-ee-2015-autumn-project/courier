34beb89613f336dff065804ffe797224c2621679
Nándor Holozsnyák <holi60@gmail.com>
2015-12-12 19:36:52 +0100

    BaseEntity refactor, 4 new rown added to each table

--- app/core/src/main/java/hu/schonherz/java/training/courier/entities/BaseEntity.java
+++ app/core/src/main/java/hu/schonherz/java/training/courier/entities/BaseEntity.java
@@ -1,5 +1,7 @@
 package hu.schonherz.java.training.courier.entities;
 
+import java.util.Date;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +12,42 @@ public class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	private Date RegDate;
+	private Date ModDate;
+	private Long RegUser;
+	private Long ModUser;
+
+	public Date getRegDate() {
+		return RegDate;
+	}
+
+	public void setRegDate(Date regDate) {
+		RegDate = regDate;
+	}
+
+	public Date getModDate() {
+		return ModDate;
+	}
+
+	public void setModDate(Date modDate) {
+		ModDate = modDate;
+	}
+
+	public Long getRegUser() {
+		return RegUser;
+	}
+
+	public void setRegUser(Long regUser) {
+		RegUser = regUser;
+	}
+
+	public Long getModUser() {
+		return ModUser;
+	}
+
+	public void setModUser(Long modUser) {
+		ModUser = modUser;
+	}
 
 	public Long getId() {
 		return id;
