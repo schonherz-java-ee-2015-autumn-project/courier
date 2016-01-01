017297bbeb03a74c2b8f574429a329b043339d94
Nándor Holozsnyák <holi60@gmail.com>
2016-01-01 22:39:26 +0100

    deleted stuffs

--- app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/QuartzManagerLocal.java
+++
@@ -1,9 +0,0 @@
-package hu.schonherz.java.training.courier.service;
-
-import javax.ejb.Local;
-
-@Local
-public interface QuartzManagerLocal {
-	public void initialize(String cronExpr, String methodName);
-    public void shutdown();
-}
--- app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/QuartzManagerRemote.java
+++
@@ -1,9 +0,0 @@
-package hu.schonherz.java.training.courier.service;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface QuartzManagerRemote {
-	public void initialize(String cronExpr,String methodName);
-    public void shutdown();
-}
