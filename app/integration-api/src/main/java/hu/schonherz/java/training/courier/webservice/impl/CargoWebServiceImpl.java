65baada8541ea096426c3530920fcf1ba70fa13a
Nándor Holozsnyák <holi60@gmail.com>
2016-01-01 22:42:49 +0100

    CargoWebServiceImpl and the two interface modded

--- app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/impl/CargoWebServiceImpl.java
+++ app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/impl/CargoWebServiceImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
 
@@ -16,22 +15,14 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.interceptor.Interceptors;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.namespace.QName;
 
 import org.apache.log4j.Logger;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
-import hu.schonherz.administration.wsserviceapi.NotAllowedRoleException_Exception;
-import hu.schonherz.administration.wsserviceapi.SynchronizationService;
-import hu.schonherz.administration.wsserviceapi.SynchronizationServiceImpl;
-import hu.schonherz.administration.wsserviceapi.UserRole;
-import hu.schonherz.administration.wsserviceapi.WebUserDTO;
 import hu.schonherz.java.training.courier.entities.CargoStatus;
 import hu.schonherz.java.training.courier.service.CargoServiceLocal;
-import hu.schonherz.java.training.courier.service.UserServiceLocal;
-import hu.schonherz.java.training.courier.service.vo.UserVO;
+import hu.schonherz.java.training.courier.service.vo.CargoVO;
 import hu.schonherz.java.training.courier.service.webservice.CargoWebServiceLocal;
 import hu.schonherz.java.training.courier.service.webservice.CargoWebServiceRemote;
 
@@ -43,7 +34,7 @@ import hu.schonherz.java.training.courier.service.webservice.CargoWebServiceRemo
 public class CargoWebServiceImpl implements CargoWebServiceLocal, CargoWebServiceRemote {
 
 	private final static Logger logger = Logger.getLogger(CargoWebServiceImpl.class);
-	
+
 	@EJB
 	CargoServiceLocal cargoServiceLocal;
 
@@ -54,9 +45,8 @@ public class CargoWebServiceImpl implements CargoWebServiceLocal, CargoWebServic
 
 	@PostConstruct
 	void init() {
-		logger.info("Starting WebServiceClient as EJB service");
-		wsdlProperties = new Properties();
 
+		wsdlProperties = new Properties();
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		try {
 			wsdlProperties.load(classLoader.getResourceAsStream("wsdllocation.properties"));
@@ -83,14 +73,40 @@ public class CargoWebServiceImpl implements CargoWebServiceLocal, CargoWebServic
 
 	@Override
 	public void getFreeCargosFromAdministration() throws Exception {
-		
+		// courierWebService majd lekéri az admin modultól a szállításokat.
+		List<CargoVO> cargosInDB = cargoServiceLocal.findAllByStatus(CargoStatus.getValue(1L));
+		updateCargos(cargosInDB);
 	}
 
 	@Override
-	public Long setCargoStatus(CargoStatus status) throws Exception {
-		return null;
+	public Long setCargoStatus(Long globalId, CargoStatus status) throws Exception {
+		//Ide jön majd a státusz update az admin moduk felé, egyenlõre hogy ne legyen baj minden sikeres
+		return (long) 0;
 	}
 
-	
+	public void updateCargos(List<CargoVO> cargosInDB /*,List<CargoWebVO> cargosInWS*/ ) {
+		Integer newCargos = 0;
+		CargoVO newCargo;
+		
+		List<Long> existingIds = new ArrayList<>();
+		for (CargoVO dbCargo : cargosInDB) {
+			existingIds.add(dbCargo.getGlobalid());
+		}
+		
+		// amint megkaptuk az admin modultól az implementációt azonnal beiktatjuk
+//		for (CargoVO wsCargo : cargoListFromWS) {
+//
+//			if (!existingIds.contains((Long) wsCargo.getGlobalid())) {
+//				try {
+//					newCargo = cargoServiceLocal.save(wsCargo);
+//					newCargos++;
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//					logger.error("Error:" + e.getMessage());
+//				}
+//			}
+//		}
+//		System.out.println("Update Success! New Cargos: " + newCargos);
+	}
 
 }
--- app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceLocal.java
+++ app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceLocal.java
@@ -1,5 +1,9 @@
 package hu.schonherz.java.training.courier.service.webservice;
 
+import hu.schonherz.java.training.courier.entities.CargoStatus;
+
 public interface CargoWebServiceLocal {
 	void getFreeCargosFromAdministration() throws Exception;
+	
+	Long setCargoStatus(Long globalId, CargoStatus status) throws Exception;
 }
