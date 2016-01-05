f374c2f2f06091dc6cfe32f574b697000fd6c0a1
Nándor Holozsnyák <holi60@gmail.com>
2016-01-05 22:37:18 +0100

    RemoteCargoConveter changed

--- app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemoteCargoConverter.java
+++ app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemoteCargoConverter.java
@@ -2,13 +2,17 @@ package hu.schonherz.java.training.courier.webservice.converters;
 
 import javax.ejb.EJB;
 
+import org.apache.log4j.Logger;
+
 import hu.schonherz.administrator.RemoteCargoDTO;
-import hu.schonherz.java.training.courier.entities.Restaurant;
+import hu.schonherz.java.training.courier.service.RestaurantServiceLocal;
 import hu.schonherz.java.training.courier.service.UserServiceLocal;
 import hu.schonherz.java.training.courier.service.vo.CargoVO;
+import hu.schonherz.java.training.courier.service.vo.RestaurantVO;
+import hu.schonherz.java.training.courier.service.vo.UserVO;
 
 public class RemoteCargoConverter {
-
+	final static Logger logger = Logger.getLogger(RemoteCargoConverter.class);
 	// RemoteCargoDTO
 	// protected Long courierId;
 	// protected Long id;
@@ -19,20 +23,30 @@ public class RemoteCargoConverter {
 
 	@EJB
 	UserServiceLocal userService;
-	
+
+	@EJB
+	RestaurantServiceLocal restaurantService;
+
 	public CargoVO toLocalVO(RemoteCargoDTO remoteCargoDTO) {
+		UserVO courier = null;
+		try {
+			courier = userService.findByGlobalId(remoteCargoDTO.getCourierId());
+		} catch (Exception e) {
+			logger.info("ERROR:", e);
+		}
+		RestaurantVO restaurant = null;
+		try {
+			restaurant = restaurantService.findRestaurantByGlobalid(remoteCargoDTO.getRestaurantId());
+		} catch (Exception e) {
+			logger.info("ERROR:", e);
+		}
 		CargoVO localVo = new CargoVO();
 		localVo.setAddresses(RemoteOrderConverter.toLocalVo(remoteCargoDTO.getOrders()));
 		localVo.setGlobalid(remoteCargoDTO.getId());
 		localVo.setStatus(RemoteCargoStateConverter.toLocalCargoState(remoteCargoDTO.getState()));
-		try {
-			localVo.setUser(userService.findByGlobalId(remoteCargoDTO.getCourierId()));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return null;
+		localVo.setRestaurant(restaurant);
+		localVo.setUser(courier);
+		return localVo;
 
 	}
 }
