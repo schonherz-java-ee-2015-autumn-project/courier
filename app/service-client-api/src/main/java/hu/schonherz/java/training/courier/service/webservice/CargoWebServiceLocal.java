f96901653383e57cef3fa23ad978ba617c89dd16
Nándor Holozsnyák <holi60@gmail.com>
2016-01-09 14:44:15 +0100

    changes is CargoWebService interfaces

--- app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceLocal.java
+++ app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceLocal.java
@@ -1,10 +1,19 @@
 package hu.schonherz.java.training.courier.service.webservice;
 
+import hu.schonherz.java.training.courier.entities.AddressStatus;
 import hu.schonherz.java.training.courier.entities.CargoStatus;
+import hu.schonherz.java.training.courier.entities.Payment;
+
 public interface CargoWebServiceLocal {
 	void getFreeCargosFromAdministration() throws Exception;
-	
+
 	Long setCargoStatus(Long globalId, CargoStatus status) throws Exception;
-	
-	Long assignUserToCargo(Long userId, Long cargoId) throws Exception; 
+
+	Long assignUserToCargo(Long userId, Long cargoId) throws Exception;
+
+	Long changeDeliveryState(Long addressId, Long userId, AddressStatus status) throws Exception;
+
+	Long changePaymentState(Long userId, Long addressId, Payment payment) throws Exception;
+
+	Long changeCargoState(Long cargoId, Long userId, CargoStatus cargoStatus) throws Exception;
 }
--- app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceRemote.java
+++ app/service-client-api/src/main/java/hu/schonherz/java/training/courier/service/webservice/CargoWebServiceRemote.java
@@ -1,6 +1,8 @@
 package hu.schonherz.java.training.courier.service.webservice;
 
+import hu.schonherz.java.training.courier.entities.AddressStatus;
 import hu.schonherz.java.training.courier.entities.CargoStatus;
+import hu.schonherz.java.training.courier.entities.Payment;
 
 public interface CargoWebServiceRemote {
 	void getFreeCargosFromAdministration() throws Exception;
@@ -8,4 +10,12 @@ public interface CargoWebServiceRemote {
 	Long setCargoStatus(Long globalId, CargoStatus status) throws Exception;
 
 	Long assignUserToCargo(Long userId, Long cargoId) throws Exception;
+
+	void testEJB();
+
+	Long changeDeliveryState(Long addressId, Long userId, AddressStatus status) throws Exception;
+
+	Long changePaymentState(Long userId, Long addressId, Payment payment) throws Exception;
+
+	Long changeCargoState(Long cargoId, Long userId, CargoStatus cargoStatus) throws Exception;
 }
