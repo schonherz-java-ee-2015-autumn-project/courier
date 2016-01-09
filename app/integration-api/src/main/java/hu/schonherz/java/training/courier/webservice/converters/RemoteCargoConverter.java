1d10bb3e45cb9d8369cb6f95e6544a1a50a852ba
Nándor Holozsnyák <holi60@gmail.com>
2016-01-09 10:41:22 +0100

    converters modded

--- app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemoteCargoConverter.java
+++ app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemoteCargoConverter.java
@@ -1,6 +1,9 @@
 package hu.schonherz.java.training.courier.webservice.converters;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 import org.apache.log4j.Logger;
 
@@ -11,6 +14,8 @@ import hu.schonherz.java.training.courier.service.vo.CargoVO;
 import hu.schonherz.java.training.courier.service.vo.RestaurantVO;
 import hu.schonherz.java.training.courier.service.vo.UserVO;
 
+@Startup
+@Singleton
 public class RemoteCargoConverter {
 	final static Logger logger = Logger.getLogger(RemoteCargoConverter.class);
 	// RemoteCargoDTO
@@ -26,26 +31,53 @@ public class RemoteCargoConverter {
 
 	@EJB
 	RestaurantServiceLocal restaurantService;
-
+	
+	@PostConstruct
+	public void init() {
+		logger.info("INFO: RemoteCargoConverter Singleton EJB started");
+		if(userService == null  || restaurantService == null) 
+		{
+			logger.info("INFO:userService or restaurantService is null.");
+		}
+	}
+	
 	public CargoVO toLocalVO(RemoteCargoDTO remoteCargoDTO) {
+		logger.info("INFO: RemoteCargoConverter toLocalVO method asked.");
 		UserVO courier = null;
 		try {
+			logger.info("INFO: userService.findByGlobalId(remoteCargoDTO.getCourierId()) running");
 			courier = userService.findByGlobalId(remoteCargoDTO.getCourierId());
+			if(courier == null) {
+				logger.info("INFO: courier is still null after userService");
+			}
 		} catch (Exception e) {
 			logger.info("ERROR:", e);
 		}
 		RestaurantVO restaurant = null;
 		try {
+			logger.info("INFO: restaurantService.findRestaurantByGlobalid(remoteCargoDTO.getRestaurantId()) running");
 			restaurant = restaurantService.findRestaurantByGlobalid(remoteCargoDTO.getRestaurantId());
+			if(restaurant == null) {
+				logger.info("INFO: Restaurant is still null after restaurantService");
+			}
+				
 		} catch (Exception e) {
 			logger.info("ERROR:", e);
 		}
+		logger.info("INFO: Making localVO");
 		CargoVO localVo = new CargoVO();
+		logger.info("INFO: CargoVO localVO made");
 		localVo.setAddresses(RemoteOrderConverter.toLocalVo(remoteCargoDTO.getOrders()));
+		logger.info("INFO: address is set in localVO");
 		localVo.setGlobalid(remoteCargoDTO.getId());
+		logger.info("INFO: globalid is set in localVO");
 		localVo.setStatus(RemoteCargoStateConverter.toLocalCargoState(remoteCargoDTO.getState()));
+		logger.info("INFO: status is set in localVO");
 		localVo.setRestaurant(restaurant);
+		logger.info("INFO: restaurant is set in localVO");
 		localVo.setUser(courier);
+		logger.info("INFO: courier is set in localVO");
+		logger.info("INFO: Returning localVO!");
 		return localVo;
 
 	}
--- app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemotePaymentConverter.java
+++ app/integration-api/src/main/java/hu/schonherz/java/training/courier/webservice/converters/RemotePaymentConverter.java
@@ -1,8 +1,6 @@
 package hu.schonherz.java.training.courier.webservice.converters;
 
-import hu.schonherz.administrator.DeliveryStateWeb;
 import hu.schonherz.administrator.RemotePaymentMethod;
-import hu.schonherz.java.training.courier.entities.AddressStatus;
 import hu.schonherz.java.training.courier.entities.Payment;
 
 public class RemotePaymentConverter {
@@ -11,12 +9,12 @@ public class RemotePaymentConverter {
 		Payment localPayment = null;
 		switch (remotePayment) {
 		case CASH:
-			localPayment = Payment.Card;
+			localPayment = Payment.Cash;
 			break;
 		case CREDIT_CARD:
 			localPayment = Payment.Card;
 			break;
-		case SZÉP_CARD:
+		case SZEP_CARD:
 			localPayment = Payment.SZEP;
 			break;
 		case VOUCHER:
